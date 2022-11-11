674950c2e841fa071560eeb353d5ff286191e916
PIRAPORN PRASERT <sasayaza@gmail.com>
2022-11-11 17:53:24 +0700

    Update LetterFrequency.py
    ใช้ dict แยกก็ได้

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -3,4 +3,16 @@ def main():
     """ I want you to be my last
 If I had to keep being separated from you like this
 I would rather die """
-    fds = input()
+    longsen = input().lower()
+    fon = "0123456789'/<>,.?()-+*&^%$#@:!;=\\ฺ{}_~[] "
+    for eyo in fon:
+        longsen = longsen.replace(eyo, "")
+    cutlongsen = (" ".join(longsen)).split(" ")
+    nobi, letter = 0, ""
+    for i in set(cutlongsen):
+        amount = longsen.count(i)
+        if amount > nobi:
+            nobi = amount
+            letter = i
+    print(letter)
+main()
