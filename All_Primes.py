da3e6738496628664c2c5b552b0c391725bdfa10
PIRAPORN PRASERT <sasayaza@gmail.com>
2022-11-11 17:59:39 +0700

    Update All_Primes.py

--- All_Primes.py
+++ All_Primes.py
@@ -1,12 +1,15 @@
-""" All_Primes """
-
+"""All_Primes"""
 def main():
-    """ All_Primes """
-    numfonnarak = int(input())
-    prime = 0
-    for i in range(2, numfonnarak):
-        for a in range(2, i):
-            if i % a != 0:
-                prime += 1
-    print(prime)
+    """All_Primes"""
+    num = int(input())
+    result = 0
+    for i in range(2, num+1):
+        x_val = 0
+        for j in range(2, i):
+            if i%j == 0:
+                x_val = 1
+                break
+        if x_val != 1:
+            result += 1
+    print(result)
 main()
