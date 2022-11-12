c53c85eb277f484f8efb4ede7c53e0b2c72d4587
PIRAPORN PRASERT <sasayaza@gmail.com>
2022-11-13 01:08:32 +0700

    Update ควย.py

--- ควย.py
+++ ควย.py
@@ -1,9 +1,11 @@
-import json
- 
-def main():
-    """Main function"""
-    player = dict()
-    txt = input()
-    player.update(json.loads(txt))
-    print(player)
-main()
+def nToKBit(n, K=64):
+   output = [0]*K
+
+   def loop(n, i):
+       if n == 0: 
+           return output
+       output[-i] = n & 1
+       return loop(n >> 1, i+1)
+
+   return loop(n, 1)
+nToKBit(int(input()))
