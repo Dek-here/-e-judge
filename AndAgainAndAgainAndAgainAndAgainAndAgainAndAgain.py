b1a341043a542dfd4bcee07b991987d2492e5298
PIRAPORN PRASERT <sasayaza@gmail.com>
2022-11-13 19:31:14 +0700

    Update AndAgainAndAgainAndAgainAndAgainAndAgainAndAgain.py

--- AndAgainAndAgainAndAgainAndAgainAndAgainAndAgain.py
+++ AndAgainAndAgainAndAgainAndAgainAndAgainAndAgain.py
@@ -1,5 +1,17 @@
 """ AndAgainAndAgainAndAgainAndAgainAndAgainAndAgain """
 def main():
     """ AndKuyAndKuyAndKuyAndKuyAndKuyAndKuyAndKuy """
-    aeiou = input()
-    
+    text = input()
+    aeiou = "aeiou"
+    total, count = 0, 0
+    text_list = text.replace(".", "").split(" ")
+    for i in sorted(text_list):
+        for k in aeiou:
+            total += i.count(k)
+        if total >= 2:
+            print(i)
+            count += 1
+        total = 0
+    if count == 0:
+        print("Nope")
+main()
